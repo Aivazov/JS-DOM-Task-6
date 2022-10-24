dc14381d5724358d2821b368d84114e65d9f48c5
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2022-10-24 22:34:07 +0300

    Final commit. Fixing task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,61 +1,21 @@
 const refs = {
-  ulCategories: document.querySelectorAll('#categories'),
   listCategories: document.querySelectorAll('.item'),
-  listTitle: document.querySelectorAll('.item > h2'),
-  listEl: document.querySelectorAll('.item > ul'),
-  ulListEl: document.querySelectorAll('.item > ul > li'),
 };
 
-const el = document.querySelector('.item > ul');
+categoriesIteration();
 
-console.log(refs.listEl);
+refs.listCategories.forEach((el) => {
+  const listTitles = el.querySelector('.item > h2').textContent;
+  const listItems = el.querySelectorAll('li').length;
 
-let res = 0;
-
-refs.listEl.forEach((el, index) => {
-  console.log(el);
-  el.forEach(el2 => el2)
-  res = index + 1;
+  console.log(`Category: ${listTitles}`);
+  console.log(`Elements: ${listItems}`);
 });
 
-console.log(res)
-
-function categoriesNumber(param, param2) {
-  let result = 0;
-  let li = 0;
-
-  [...param].forEach((el, index) => {
-    console.log(el.textContent)
-    result = index + 1;
-    // console.log(`el: ${el.textContent} has index ${index}`);
-  });
-  return result;
-}
-
-function iterateListEl() {
+function categoriesIteration() {
   let result = 0;
   refs.listCategories.forEach((el, index) => {
     result = index + 1;
   });
   console.log(`Number of elements: ${result}`);
 }
-
-function categoriesSelection() {
-  [...refs.listTitle].forEach((el, index) =>
-    console.log(`Category: ${el.textContent}
-Elements: ${categoriesNumber(refs.ulListEl)}`)
-  );
-}
-
-// function categoriesSelection2(number) {
-//   [...refs.listTitle].forEach((el) => {
-//     [...refs.ulListEl].forEach((el) =>
-//       console.log(`Category: ${el.textContent}
-// Elements: ${categoriesNumber(refs.ulListEl)}`)
-//     );
-//   });
-// };
-// iterateListEl();
-// categoriesSelection();
-// categoriesSelection2();
-// console.log('');
